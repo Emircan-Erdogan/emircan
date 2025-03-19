34404221d7bea3bb086e8e2ed505c3f13de7c013
Emircan <246001043@stu.adu.edu.tr>
2025-03-19 12:02:55 +0300

    Ders-3 eklendi

---
+++ Ders-3/Ders-3b.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h3> Global Değişkenler</h3>
+    <h4>$_POST</h4>
+
+    <!-- <form action="" method="Get"> 
+
+        <label for="kullanici">Kullanıcı Adı:</label>
+        <input type="text" name="username" id="kullanici">
+        <br>
+        <label for="sifre">Şifre:</label>
+        <input type="password" name="password" id="sifre">
+        <br>
+        <input type="submit" value="Giriş Yap">
+
+    </form>
+
+
+<?php
+
+    $kullanici_adi      = $_GET["username"];
+    $kullanici_sifresi  = $_GET["password"];
+
+    echo "Formdan Kullanıcı Adı:     $kullanici_adi <br>";
+    echo "Formdan Kullanıcı Şifresi: $kullanici_sifresi <br>";
+
+
+?> -->
+
+
+
+<form action="" method="POST"> 
+
+<label for="kullanici">Kullanıcı Adı:</label>
+<input type="text" name="username" id="kullanici">
+<br>
+<label for="sifre">Şifre:</label>
+<input type="password" name="password" id="sifre">
+<br>
+<input type="submit" value="Giriş Yap">
+
+</form>
+
+
+<?php
+
+
+
+$kullanici_adi      = @$_POST["username"];
+$kullanici_sifresi  = @$_POST["password"];
+
+echo "Formdan Kullanıcı Adı:     $kullanici_adi <br>";
+echo "Formdan Kullanıcı Şifresi: $kullanici_sifresi <br>";
+
+if ($kullanici_adi == "Ali" && $kullanici_sifresi == 123)
+
+{
+    echo "Kullanıcı Adı ve Şifresi Doğru";
+}
+
+?>
+
+</body>
+</html>
---
+++ Ders-3/Ders-3d2.php
@@ -0,0 +1,19 @@
+<?php
+
+    $gelenDosya = $_FILES["cv"];
+
+    echo "<pre>";
+    print_r($gelenDosya);
+    echo "</pre>";
+
+
+
+    echo "Gelen Dosyanın Adı: " . $gelenDosya["name"] . "<br>";
+    echo "Gelen Dosyanın Mi Türü: " . $gelenDosya["type"] . "<br>";
+    echo "Gelen Dosyanın Temp Dizini: " . $gelenDosya["tmp_name"] . "<br>";
+    echo "Gelen Dosyanın Boyutu: " . $gelenDosya["size"] . "<br>";
+
+
+
+
+?>
